eb6b88e8f88b769c2b0bdf4ad7aa9c093b27f951
AsianRiceX <jtbow1111@gmail.com>
2023-10-25 14:11:37 -0400

    completed encoding and decoding aspects

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 def main():
     print("-------------\n1. Encode\n2. Decode\n2. Decode\n")
     user_choice = int(input("Please enter an option: "))
-
+    encoded_pass = ""
+    user_pass = ""
     if user_choice == 1:
         user_pass = input("Please enter your password to encode: ")
+        for x in range(len(user_pass)):
+            encoded_pass += str(int(user_pass[x]) + 3)
         print("Your password has been encoded and stored!")
     elif user_choice == 2:
-        print("")
+        print("The encoded password is " + encoded_pass + ", and the original password is " + user_pass)
     elif user_choice == 3:
         exit()
