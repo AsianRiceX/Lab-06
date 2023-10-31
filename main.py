e3e419e2d25df90bd4e7be8518d083519ab16b88
AsianRiceX <jtbow1111@gmail.com>
2023-10-31 18:29:09 -0400

    changed formatting to use functions, completed encode

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+def encode(password):
+    temp_pass = ""
+    for x in range(len(password)):
+        temp_pass += str(int(password[x]) + 3)
+    print("Your password has been encoded and stored!")
+    return temp_pass
+
+
+def decode(password):
+    return -1
+
+
 def main():
     print("-------------\n1. Encode\n2. Decode\n2. Decode\n")
     user_choice = int(input("Please enter an option: "))
@@ -5,10 +17,9 @@ def main():
     user_pass = ""
     if user_choice == 1:
         user_pass = input("Please enter your password to encode: ")
-        for x in range(len(user_pass)):
-            encoded_pass += str(int(user_pass[x]) + 3)
-        print("Your password has been encoded and stored!")
+        encoded_pass = encode(user_pass)
     elif user_choice == 2:
-        print("The encoded password is " + encoded_pass + ", and the original password is " + user_pass)
+        decoded_pass = decode(encoded_pass)
+        print("")
     elif user_choice == 3:
         exit()
