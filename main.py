6ed6cbc0823bfbb3ba70d16c3e64e29a1eceb1f7
AsianRiceX <149002461+AsianRiceX@users.noreply.github.com>
2023-10-31 20:51:02 -0400

    Updated main to work with files and commented

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
-def encode(password):
-    temp_pass = ""
-    for x in range(len(password)):
-        temp_pass += str(int(password[x]) + 3)
-    print("Your password has been encoded and stored!")
-    return temp_pass
-
-
-def decode(password):
-    return -1
+import encode
+import decode
 
 
 def main():
-    print("-------------\n1. Encode\n2. Decode\n2. Decode\n")
-    user_choice = int(input("Please enter an option: "))
+    # placeholders
     encoded_pass = ""
-    user_pass = ""
-    if user_choice == 1:
-        user_pass = input("Please enter your password to encode: ")
-        encoded_pass = encode(user_pass)
-    elif user_choice == 2:
-        decoded_pass = decode(encoded_pass)
-        print("")
-    elif user_choice == 3:
-        exit()
+    decoded_pass = ""
+    while True:
+        print("-------------\n1. Encode\n2. Decode\n3. Quit\n")
+        user_choice = int(input("Please enter an option: "))
+        # encode a password using another file
+        if user_choice == 1:
+            user_pass = input("Please enter your password to encode: ")
+            encoded_pass = encode.enc(user_pass)
+        # decode an encoded password using another file
+        elif user_choice == 2:
+            decoded_pass = decode.dec(encoded_pass)
+            print("The encoded password is " + encoded_pass + ", and the original password is " + decoded_pass + ".")
+        # exit the code
+        elif user_choice == 3:
+            exit()
+
+
+if __name__ == '__main__':
+    main()
